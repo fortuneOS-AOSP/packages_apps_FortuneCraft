c8316bc2376208cf69d5bc79a107f050103a1089
Pranav Vashi <neobuddy89@gmail.com>
2023-11-25 16:11:53 +0000

    Craft: Add omnijaws lockscreen customization
    Change-Id: If2c93f01b9b68392b2999d0ed030c48f0091efe9

--- src/org/fortune/craft/category/CraftLockScreen.java
+++ src/org/fortune/craft/category/CraftLockScreen.java
@@ -11,11 +11,17 @@ import android.os.Bundle;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.fortune.OmniJawsClient;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
 public class CraftLockScreen extends SettingsPreferenceFragment
     implements Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_WEATHER = "lockscreen_weather_enabled";
+
+    private Preference mWeather;
+    private OmniJawsClient mWeatherClient;
 
   @Override
   public void onCreate(Bundle icicle) {
@@ -25,6 +31,10 @@ public class CraftLockScreen extends SettingsPreferenceFragment
     ContentResolver resolver = getActivity().getContentResolver();
     final PreferenceScreen prefScreen = getPreferenceScreen();
     Resources resources = getResources();
+
+    mWeather = (Preference) findPreference(KEY_WEATHER);
+    mWeatherClient = new OmniJawsClient(getContext());
+    updateWeatherSettings();
   }
 
   public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -32,6 +42,15 @@ public class CraftLockScreen extends SettingsPreferenceFragment
 
     return false;
   }
+
+  private void updateWeatherSettings() {
+        if (mWeatherClient == null || mWeather == null) return;
+
+        boolean weatherEnabled = mWeatherClient.isOmniJawsEnabled();
+        mWeather.setEnabled(weatherEnabled);
+        mWeather.setSummary(weatherEnabled ? R.string.lockscreen_weather_summary :
+            R.string.lockscreen_weather_enabled_info);
+    }
 
   @Override
   public int getMetricsCategory() {
